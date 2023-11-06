e3bac8fb8c3e1f363699af228a2fefd75d7cd3c2
EnshinBank <enshinroundcube@gmail.com>
2023-11-06 14:30:25 +0900

    third comit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 require("dotenv")
 
 const app = express();
@@ -7,9 +8,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
 
-app.get("/", (req, res) => { res.status(200).send("Hello, World!");})
+// app.get("/", (req, res) => { res.status(200).send("Hello, World!");})
 
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 
